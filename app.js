5eb87087020045e4f5c6cc3490d0f7f757c7df0e
guilhermecolares <guilhermegraf17@gmail.com>
2025-03-18 13:13:54 -0300

    comit

---
+++ app.js
@@ -0,0 +1,11 @@
+// IMPORT DE BIBLIOTECAS
+    const express = require('express')
+    const handlebars = require('express-handlebars')
+    const bodyParser = require('body-parser')
+    //const mongoose = require('mongoose')
+
+// CONFIGS
+
+// ROTAS
+
+//OUTROS
