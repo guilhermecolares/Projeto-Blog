721139148797f3a5900ae2683dcfafdf66375957
guilhermecolares <guilhermegraf17@gmail.com>
2025-03-19 10:09:08 -0300

    add de bootstrap

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 // IMPORT DE BIBLIOTECAS
-    const express = require('express')
-    const handlebars = require('express-handlebars')
-    const bodyParser = require('body-parser')
+    import express from 'express'
+    import { engine } from 'express-handlebars'
+    const app = express()
+    import admin from './routes/admin.js'
     //const mongoose = require('mongoose')
 
 // CONFIGS
+    //BODY PARSER
+    app.use(express.urlencoded({ extended: true }))
+    app.use(express.json())
+
+    // HANBLEBARS
+    app.engine('handlebars', engine({ defaultLayout: 'main' }));
+    app.set('view engine', 'handlebars');
+
+    // MONGOOSE
+
 
 // ROTAS
+    app.use('/admin', admin)
 
 //OUTROS
+const PORT = 9091
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
