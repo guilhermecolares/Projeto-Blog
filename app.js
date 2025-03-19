dfa764cbee94541a2ff1d2c4a4e87b11ccd9f050
guilhermecolares <guilhermegraf17@gmail.com>
2025-03-19 14:47:23 -0300

    finalização do dia 19/03

--- app.js
+++ app.js
@@ -6,42 +6,65 @@
         import { fileURLToPath } from 'url'
     import admin from './routes/admin.js'
     import mongoose from 'mongoose'
+    import session from 'express-session'
+    import flash from 'connect-flash'
+
     const app = express()
 
     const __filename = fileURLToPath(import.meta.url)
     const __dirname = path.dirname(__filename)
 
 // CONFIGS
-    //BodyParser
-    app.use(express.urlencoded({ extended: true }))
-    app.use(express.json())
+    // Session
+        app.use(session({
+            secret: 'cursodenodejs',
+            resave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash())
+
+    // Middlewares
+        app.use((req, res, next) => {
+            res.locals.success_msg = req.flash('success_msg')
+            res.locals.error_msg = req.flash('error_msg')
+            next()
+        })
+
+    // BodyParser
+        app.use(express.urlencoded({ extended: true }))
+        app.use(express.json())
 
     // Handlebars
-    app.engine('handlebars', engine({ defaultLayout: 'main' }));
-    app.set('view engine', 'handlebars');
-    app.set('views', path.join(__dirname, 'views'))
+        app.engine('handlebars', engine({ 
+            defaultLayout: 'main',
+            runtimeOptions: {
+                allowProtoPropertiesByDefault: true
+            }
+        }));
+        app.set('view engine', 'handlebars');
+        app.set('views', path.join(__dirname, 'views'));
 
     // Public
-    app.use(express.static(path.join(__dirname, 'public')))
+        app.use(express.static(path.join(__dirname, 'public')))
 
-    app.use((req, res, next) => {
-        console.log('Middleware')
-        next()
+        app.use((req, res, next) => {
+            console.log('Middleware')
+            next()
     })
 
     // Mongoose
-    mongoose.Promise = global.Promise
-    mongoose.connect('mongodb://localhost/blog').then(() => {
-        console.log('Conectado ao Banco de Dados (MongoDB)')
-    }).catch((err) => {
-        console.log(`Houve um erro ao se conectar ao banco de dados: ${err}`)
-    })
+        mongoose.Promise = global.Promise
+        mongoose.connect('mongodb://localhost/blog').then(() => {
+            console.log('Conectado ao Banco de Dados (MongoDB)')
+        }).catch((err) => {
+            console.log(`Houve um erro ao se conectar ao banco de dados: ${err}`)
+        })
 
 // ROTAS
     app.use('/admin', admin)
 
-//OUTROS
-const PORT = 9091
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`)
-})
+// OUTROS
+    const PORT = 9091
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando na porta ${PORT}`)
+    })
--- routes/admin.js
+++ routes/admin.js
@@ -13,7 +13,12 @@ router.get('/posts', (req, res) => {
 })
 
 router.get('/categorias', (req, res) =>{
-    res.render('admin/categorias')
+    Categoria.find().sort({date: 'desc'}).then((categorias) => {
+        res.render('admin/categorias', {categorias})
+    }).catch((err) => {
+        req.flash('error_msg', 'Houve um erro ao listar categorias, tente novamente!')
+        res.redirect('/admin')
+    })
 })
 
 router.get('/categorias/add', (req, res) => {
@@ -21,16 +26,37 @@ router.get('/categorias/add', (req, res) => {
 })
 
 router.post('/categorias/nova', (req, res) =>{
-    const novaCategoria = {
-        nome: req.body.nome,
-        slug: req.body.slug
+
+    var erros = []
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: 'Campo "Nome" vazio!'})
     }
 
-    new Categoria(novaCategoria).save().then(() => {
-        console.log('Categoria salva com sucesso!')
-    }).catch((err) => {
-        console.log(`Falha ao salvar categoria: ${err}`)
-    })
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+        erros.push({texto: 'Campo "Slug" vazio!'})
+    }
+
+    if(req.body.nome.length < 2 || req.body.slug.length < 2){
+        erros.push({texto: 'Número de caracteres insuficiente!'})
+    }
+
+    if(erros.length > 0){
+        res.render('admin/addcategorias', {erros})
+    } else {
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+    
+        new Categoria(novaCategoria).save().then(() => {
+            req.flash('success_msg', 'Categoria criada com sucesso!')
+            res.redirect('/admin/categorias')
+        }).catch((err) => {
+            req.flash('error_msg', 'Erro ao criar categoria, tente novamente!')
+            res.redirect('/admin')
+        })
+    }
 })
 
 export default router;
