512781bccab5f44f5c670bac45a70a5b524f2278
guilhermecolares <guilhermegraf17@gmail.com>
2025-03-20 11:51:20 -0300

    att

--- routes/admin.js
+++ routes/admin.js
@@ -59,4 +59,70 @@ router.post('/categorias/nova', (req, res) =>{
     }
 })
 
+router.get('/categorias/edit/:id', (req, res) => {
+    Categoria.findOne({_id: req.params.id}).then((categoria) => {
+        res.render('admin/editcategorias', {categoria})
+    }).catch((err) => {
+        req.flash('error_msg', 'Erro ao editar categoria, tente novamente!')
+        res.redirect('/admin/categorias')
+    })  
+})
+
+router.post('/categorias/edit', (req, res) => {
+    Categoria.findOne({_id: req.body.id}).then((categoria) => {
+        const { nome, slug } = req.body
+
+        var erros = []
+
+        if(!nome || nome == undefined || nome == null) {
+            erros.push({texto: 'Campo "nome" vazio!'})
+        }
+
+        if(!slug || slug == undefined || req.body.slug == null) {
+            erros.push({texto: 'Campo "slug" vazio!'})
+        }
+
+        if(nome.length < 2 || slug.length < 2) {
+            erros.push({texto: 'Número de caracteres insuficiente!'})
+        }
+
+        if(erros.length > 0) {
+            return res.render('admin/editcategorias', {categoria: categoria, erros})
+        } else {
+            categoria.nome = req.body.nome
+            categoria.slug = req.body.slug
+            return categoria.save().then(() => {
+                req.flash('success_msg', 'Categoria editada com sucesso!')
+                res.redirect('/admin/categorias')
+            }).catch((err) => {
+                req.flash('error_msg', 'Erro ao editar categoria, tente novamente!')
+                res.redirect('/admin/categorias')
+            })
+        }
+    }).catch((err) => {
+        req.flash('error_msg', 'Erro ao editar categoria, tente novamente!')
+        res.redirect('/admin/categorias')
+    })
+})
+
+router.post('/categorias/deletar', (req, res) => {
+    const { id } = req.body
+    
+    var erros = []
+
+    if(!id || id == undefined || id == null) {
+        
+        erros.push({texto: 'Categoria não encontrada, tente novamente!'})
+    }
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+
+    }
+
+
+
+
+    Categoria.remove({_id: req.body.id}).then(() => {})
+})
+
 export default router;
